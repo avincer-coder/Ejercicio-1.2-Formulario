5066bc5b9566bd6e017349638a7f644ddea07d31
Mario <marioeduardo.garciasalazar@gmail.com>
2023-09-06 15:40:40 -0600

    Formulario terminado

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import './App.css';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faLocationDot} from "@fortawesome/free-solid-svg-icons";
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <form className='contenedor_general'>
+      <h1 className='title tipo_letra'>Discover what's out there.</h1>
+      <section className='buscador_y_btn'>
+        <div className='inputext_location_icon'>
+          <div className='icon'>
+            <FontAwesomeIcon icon={faLocationDot}/>
+          </div>
+          <input type="text" placeholder='Where do you want to go?' className='input_text tipo_letra'></input>
+        </div>
+        <button className='btn_search tipo_letra'>Search</button>
+      </section>
+    </form>
   )
 }
 
